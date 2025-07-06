3abf7fc9b1e5463620273793a6ba655e984e5a82
LZWcc <l1274645232@gmail.com>
2025-07-06 22:18:01 +0800

    finish:lab02

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -8,16 +8,20 @@ public class BombMain {
         if (args.length > 0) {
             phase = Integer.parseInt(args[0]);
         }
+        // TODO: 使用调试技巧找出每个阶段的正确输入（密码）
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            // TODO: 找出 phase0 的正确输入
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            // TODO: 找出 phase1 的正确输入
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            // TODO: 找出 phase2 的正确输入
+            b.phase2("793227803");
         }
     }
 }
